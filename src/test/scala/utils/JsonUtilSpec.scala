7fca3a3ab043b7f0ed2d625d541fb02edeaf5cbd
quentindamois <quentin.damois@efrei.net>
2025-07-19 17:47:15 +0200

    Create JsonUtilSpec.scala
    Created class JsonUtilSpec for test.

---
+++ src/test/scala/utils/JsonUtilSpec.scala
@@ -0,0 +1,13 @@
+package utils
+
+import models.LibCatalog
+import org.scalatest.BeforeAndAfter
+import org.scalatest.flatspec.AsyncFlatSpec
+
+class JsonUtilSpec extends AsyncFlatSpec with BeforeAndAfter {
+  /*
+  "JsonUtil" should "Convert a LibCatalog in a json" in {
+    JsonUtil.safeLibConvertingToJson(new LibCatalog()) should equal ()
+  }
+   */
+}
