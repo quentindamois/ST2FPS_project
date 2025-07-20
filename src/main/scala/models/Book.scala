d85912ecb9a98bc280f15ab98309f638952c3ef4
quentindamois <quentin.damois@efrei.net>
2025-07-20 18:26:50 +0200

    Update Book.scala
    Added TODO to remind that we should replace the type of author.

--- src/main/scala/models/Book.scala
+++ src/main/scala/models/Book.scala
@@ -14,7 +14,7 @@ import utils.Id
 case class Book(
   id: Id,
   title: String,
-  author: String,
+  author: String, //TODO: Change to a List[String]
   isbn: Id,
   publishedDate: LocalDate,
   genre: String,
