750a255bbe6313b4a441dc0b2afde25d6205ac3a
quentindamois <quentin.damois@efrei.net>
2025-07-25 17:09:35 +0200

    Update ErrorHandling.scala
    Added enum error for error handling the input entered by the user.

--- src/main/scala/utils/ErrorHandling.scala
+++ src/main/scala/utils/ErrorHandling.scala
@@ -37,4 +37,14 @@ object ErrorHandling {
     case AuthorisationError(file: String) extends FileError(s"$file cannot be edited")
     case ReadingError(file: String) extends FileError(s"$file cannot be read")
     case ConversionError(startingType: String, finalType: String) extends FileError(s"$startingType cannot be converted to $finalType")
+  /**
+   * An enum used to handle the error that can be caused when calling the function ManualControl
+   * */
+  enum UserError(val message: String):
+    case convertTypeError(input: String, typeOfInput: String) extends UserError(s"$input cannot be converted to $typeOfInput.")
+    case wrongLineReading(field: String) extends UserError(s"$field could not be read.")
+    case wrongInput() extends UserError("Encountered an error while entering values.")
+    case noActionSelected(userInput: String) extends UserError(s"$userInput does not correspond to any input.\n1-search title\n2-search author\n3-search genre\n4-filter availability\n5-add user\n6-add book\n7-borrow Book\n8-return Book\n9-remove Book\n10-remove user\n11-get statistic\n12-get recommendation")
+    case idNotFound(typeId: String) extends UserError(s"Id of $typeId could not be found.")
 }
+
